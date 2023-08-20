368a7dd987446975ff01c8a977feb063e36b53cb
D. Binara Kaveesha Weerasekara <119029171+D-Binara@users.noreply.github.com>
2023-08-20 23:10:13 +0530

    Update planpage.js
    update

--- navigation/planpage.js
+++ navigation/planpage.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native';
+import { View, Text ,StyleSheet} from 'react-native';
 import React from 'react';
 
 export default function planpage() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>plan</Text>
     </View>
   )
 }
+const styles = StyleSheet.create({
+
+  container:{
+    backgroundColor:"#000",
+    height:800
+  }
+
+
+})
